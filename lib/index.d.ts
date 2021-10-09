9158fb47640f3f6f196e8a516e0d4dae0569589e
David Kilson <itzkwaysi@gmail.com>
2021-10-09 10:18:22 +0100

    update build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -21,6 +21,8 @@ export declare type RenderProps = {
     reset: Action;
     resume: Action;
     data: AudioData;
+    paused?: boolean;
+    recording?: boolean;
     props?: {
         [key: string]: any;
     };
--- lib/useRecorder.d.ts
+++ lib/useRecorder.d.ts
@@ -1,14 +1,20 @@
 declare type Props = {
-    mimeTypeToUseWhenRecording: string | null;
+    mimeTypeToUseWhenRecording?: string;
 };
 export default function useRecorder({ mimeTypeToUseWhenRecording }: Props): {
-    time: import(".").Time;
+    time: {
+        h: number;
+        m: number;
+        s: number;
+    };
     reset: () => void;
     stop: () => void;
     data: import(".").AudioData;
     start: () => void;
     pause: () => void;
     resume: () => void;
+    paused: boolean;
+    recording: boolean;
 };
 export {};
 //# sourceMappingURL=useRecorder.d.ts.map
